26adf33bfb4ed29f8388445323a2a919ab23d7d7
Timo Bruderek <openvario@timoshome.org>
2020-04-06 11:08:39 -0400

    Solved variable problem

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+machine=$1
+target=$2
+
+echo "Machine to build: " $machine
+echo "Target to build: " $target
+
 export TEMPLATECONF=meta-openvario/conf
+export MACHINE="$1"
+
 . ./poky/oe-init-build-env
 
-bitbake openvario-image-testing
+bitbake $target
